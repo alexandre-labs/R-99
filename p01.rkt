54ba29a967b67bff744cfe56a96599ea98a7cff7
Alexandre <0x41e@protonmail.ch>
2016-09-18 21:34:08 -0300

    Adding problem 1

---
+++ p01.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+;; P01 (*) Find the last box of a list.
+;;     Example:
+;;     * (my-last '(a b c d))
+;;     (D)
+
+(define (my-last a-list)
+  (cond
+    [(eq? '() a-list) '()]
+    [(eq? (length a-list) 1) a-list]
+    [else (my-last (cdr a-list))]))
+
+
+(require rackunit)
+
+(check-equal? (my-last '()) '())
+(check-equal? (my-last '(1)) '(1))
+(check-equal? (my-last '(a b c d)) '(d))
