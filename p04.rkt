6bfac26a0c6089f6cce0ac5deebb967589d2b679
Alexandre <0x41e@protonmail.ch>
2016-09-18 21:58:45 -0300

    Adding problem 4

---
+++ p04.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+;; P04 (*) Find the number of elements of a list.
+
+(define (my-length a-list)
+  (cond
+    [(eq? a-list '()) 0]
+    [else (+ 1 (my-length (cdr a-list)))]))
+
+
+(require rackunit)
+
+(check-equal? (my-length '()) 0)
+(check-equal? (my-length '(1)) 1)
+(check-equal? (my-length '(1 2 3 4 5 6 7 8 9 10)) 10)
