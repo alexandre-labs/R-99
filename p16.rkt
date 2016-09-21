b54fb6cba2ef487126d25fcf1c28a5d80a120673
Alexandre <0x41e@protonmail.ch>
2016-09-21 16:25:16 -0300

    Adding problem 16 (all tests ok)

--- p16.rkt
+++ p16.rkt
@@ -5,16 +5,29 @@
 ;;     * (drop '(a b c d e f g h i k) 3)
 ;;     (A B D E G H K)
 
-(define (my-drop a-list nth)
-  (println a-list)
+(define (enumerate a-list [pos 0])
   (cond
     [(eq? a-list '()) '()]
-    [(eq? nth 0) a-list]
-    [(eq? nth 1) (cdr a-list)]
-    [else (cons (car a-list) (my-drop (cdr a-list) (- nth 1)))]))
+    [else (cons (cons (+ pos 1) (car a-list)) (enumerate (cdr a-list) (+ pos 1)))]))
+
+(define (multiple? a b)
+  (and (not (eq? a 0)) (eq? (modulo a b) 0)))
+
+(define (my-drop a-list nth)
+  (cond
+    [(eq? a-list '()) '()]
+    [else (map cdr (filter (lambda (item) (not (multiple? (car item) nth))) (enumerate a-list)))]))
 
 
 (require rackunit)
+
+(check-equal? (enumerate '()) '())
+(check-equal? (enumerate '(1)) '((1 . 1)))
+(check-equal? (enumerate '(a b c d e)) '((1 . a) (2 . b) (3 . c) (4 . d) (5 . e)))
+
+(check-false (multiple? 0 3))
+(check-true (multiple? 3 3))
+(check-true (multiple? 6 3))
 
 (check-equal? (my-drop '() 1) '())
 (check-equal? (my-drop '(1) 1) '())
