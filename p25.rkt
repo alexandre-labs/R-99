ef1b0cc60c2a7ed5b5c08769257d32481a641077
Alexandre <0x41e@protonmail.ch>
2016-09-23 17:16:30 -0300

    Adding problem 25

---
+++ p25.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+;; P25 (*) Generate a random permutation of the elements of a list.
+;;     Example:
+;;     * (rnd-permu '(a b c d e f))
+;;     (B A D C E F)
+;;
+;;     Hint: Use the solution of problem P23.
+
+(require "p23.rkt")
+
+(define (rnd-permu a-list)
+  (rnd-select a-list (length a-list)))
