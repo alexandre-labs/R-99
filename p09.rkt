fe4ac2c2c0f96c4808125213bf8fe14cb3a4b335
Alexandre <0x41e@protonmail.ch>
2016-09-25 22:02:05 -0300

    Add problem 9 (it works)

--- p09.rkt
+++ p09.rkt
@@ -7,41 +7,30 @@
 ;;     * (pack '(a a a a b c c a a d e e e e))
 ;;     ((A A A A) (B) (C C) (A A) (D) (E E E E))
 
-;; get all elements until a different one
-(define (take-while-eq a-list)
+(define (pack-aux a-list)
   (cond
     [(eq? a-list '()) '()]
     [(eq? (cdr a-list) '()) a-list]
-    [(not (eq? (car a-list) (car (cdr a-list)))) (cons (car a-list) '()) ]
-    [else (append (cons (car a-list) (take-while-eq (cdr a-list))))]))
+    [(not (eq? (car a-list) (car (cdr a-list)))) (cons (car a-list) '())]
+    [else (cons (car a-list ) (pack-aux (cdr a-list)))]))
+
+(define (pack-skip a-list)
+  (cond
+    [(eq? a-list '()) '()]
+    [(eq? (cdr a-list) '()) '()]
+    [(not (eq? (car a-list) (car (cdr a-list)))) (cdr a-list)]
+    [else (pack-skip (cdr a-list))]))
 
 
 (define (pack a-list)
   (cond
     [(eq? a-list '()) '()]
-    [(eq? (cdr a-list) '()) a-list]
-    [(not (eq? (car a-list) (car (cdr a-list)))) (append (take a-list 2) (pack (drop a-list 2)))]
-    [else (cons (take a-list 2) (pack (drop a-list 2)))]
-    ))
+    [else (cons (pack-aux a-list) (pack (pack-skip a-list)))]))
 
 
 (require rackunit)
 
-;;
-;; take-while tests
-;;
-(check-equal? (take-while-eq '()) '())
-(check-equal? (take-while-eq '(1 1)) '(1 1))
-(check-equal? (take-while-eq '(1 1 2)) '(1 1))
-(check-equal? (take-while-eq '(1 1 1 1 1 1 1 1 1 1 1 1 2)) '(1 1 1 1 1 1 1 1 1 1 1 1 ))
-(check-equal? (take-while-eq '(1 1 2 1 1)) '(1 1))
-
-
-;;
-;; pack tests
-;;
 (check-equal? (pack '()) '())
-(check-equal? (pack '(1)) '(1))
-(check-equal? (pack '(1 2)) '(1 2))
-(check-equal? (pack '(1 1 2)) '((1 1) 2))
-(check-equal? (pack '(a a a a b c c a a d e e e e)) '((a a a a) (b) (c c) (a a) (d) (e e e e)))
+(check-equal? (pack '(1)) '((1)))
+(check-equal? (pack '(1 1 2)) '((1 1) (2)))
+(check-equal? (pack '(1 1 2 1 1)) '((1 1) (2) (1 1)))
